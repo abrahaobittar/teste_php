3c599a10ba23358141321300fcfe9285a0d98ce0
Abrahao <abrahaobittar@gmail.com>
2022-11-30 17:12:27 -0300

    teste

--- law.php
+++ law.php
@@ -9,3 +9,97 @@ echo "todos tem o direito de ler e aprender";
 echo "todos tem o direito de ler e aprender";
 echo "todos tem o direito de ler e aprender";
 echo "todos tem o direito de ler e aprender";
+
+
+
+
+<?php
+
+echo "abc";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+
+
+
+
+<?php
+
+echo "abc";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+
+
+
+
+<?php
+
+echo "abc";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+
+
+
+
+<?php
+
+echo "abc";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+
+
+
+
+<?php
+
+echo "abc";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+
+
+
+
+<?php
+
+echo "abc";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+echo "todos tem o direito de ler e aprender";
+
+
+
+
